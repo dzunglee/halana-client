f29626a5cd3d3cce7700a11641a0618b500ceb88
dzunglee <dzung.lee95@gmail.com>
2022-04-12 17:40:11 +0700

    Mark as read message

--- src/modules/chat/components/Messages.vue
+++ src/modules/chat/components/Messages.vue
@@ -210,69 +210,6 @@
           v-on:keyup.enter="sendMessage"
         ></textarea>
         <div class="right-0 items-center inset-y-0 hidden sm:flex gap-2">
-          <!-- <button
-                  type="button"
-                  class="mt-auto block inline-flex items-center justify-center rounded-full h-6 w-6 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    class="h-6 w-6 text-gray-600"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-                    ></path>
-                  </svg>
-                </button>
-                <button
-                  type="button"
-                  class="mt-auto block inline-flex items-center justify-center rounded-full h-6 w-6 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    class="h-6 w-6 text-gray-600"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
-                    ></path>
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
-                    ></path>
-                  </svg>
-                </button>M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z
-                <button
-                  type="button"
-                  class="mt-auto block inline-flex items-center justify-center rounded-full h-6 w-6 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    class="h-6 w-6 text-gray-600"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    ></path>
-                  </svg>
-                </button> -->
           <button
             type="button"
             class="mt-auto block inline-flex items-center justify-center rounded-full h-6 w-6 transition duration-500 ease-in-out text-white bg-violet-600 hover:bg-blue-400 focus:outline-none"
@@ -398,14 +335,13 @@ export default defineComponent({
       })
     }
     const unSubscribe = (channel: string) => {
-      emitterClient.unSubscribe({
+      emitterClient.unsubscribe({
         key: emitterKey.value,
         channel,
       })
     }
     const handleOnType = () => {
       isTyping.value = true
-      // setTimeout(() => scrollToBottom(), 100)
       if (timeOut.value) {
         clearTimeout(timeOut.value)
       }
@@ -487,7 +423,7 @@ export default defineComponent({
 
     const readMessages = () => {
       const start = chatContainerRef.value.scrollTop
-      const end = start + chatContainerRef.value.clientHeight
+      const end = start + chatContainerRef.value.clientHeight + 20
       const unReadList = Array.prototype.slice.call(
         document.getElementsByClassName('unRead'),
       )
@@ -495,9 +431,7 @@ export default defineComponent({
         if (!msg.classList.contains('read')) {
           const itemPosition = msg.offsetTop,
             itemId = msg.getAttribute('id')
-          console.log(start, itemPosition, end)
           if (start <= itemPosition && end >= itemPosition) {
-            console.log(msg)
             msg.classList.add('read')
             readMessage(itemId)
           }
